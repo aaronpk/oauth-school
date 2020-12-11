77413dcd4c04738d114c86467ae98295545dfc7c
Aaron Parecki <aaron@parecki.com>
2020-12-10 17:18:47 -0800

    refactor exercise list into an array

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -17,14 +17,30 @@ class DefaultController extends AbstractController {
   public function index(): Response {
 
     $exercises = [
-      'introduction', 'web',
+      'client' => [
+        'introduction' => 'Getting Started', 
+        'web' => 'OAuth for Web Applications', 
+        'native' => 'OAuth for Native Applications', 
+        'spa' => 'OAuth for Single-Page Applications', 
+        'device' => 'OAuth for IoT and Smart Devices', 
+        'service' => 'OAuth for Machine-to-Machine Applications', 
+        'openid' => 'OpenID Connect',
+      ],
+      'server' => [
+        'api' => 'Protecting an API with OAuth', 
+        'revoke' => 'Revoking an Access Token', 
+        'scopes' => 'Enforcing Scopes in Your API',
+      ],
     ];
     $status = [];
-    foreach($exercises as $key) {
-      $status[$key] = $this->session->get('complete_'.$key);
+    foreach($exercises as $type=>$list) {
+      foreach($list as $key=>$name) {
+        $status[$key] = $this->session->get('complete_'.$key);
+      }
     }
 
     return $this->render('index.html.twig', [
+      'exercises' => $exercises,
       'status' => $status,
     ]);
   }
