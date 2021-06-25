c88adfa6bfec5ec80bc0c6407eab64d6d4469fdd
Aaron Parecki <aaron@parecki.com>
2021-06-24 20:40:25 -0700

    add schema for challenge

--- database/schema.sql
+++ database/schema.sql
@@ -18,3 +18,32 @@ CREATE TABLE `results` (
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `challenge_participants` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `issuer` varchar(255) DEFAULT NULL,
+  `complete` tinyint(4) DEFAULT NULL,
+  `status_active` tinyint(4) DEFAULT NULL,
+  `status_issued_before` tinyint(4) DEFAULT NULL,
+  `status_confidential` tinyint(4) DEFAULT NULL,
+  `status_lifetime` tinyint(4) DEFAULT NULL,
+  `status_scope` tinyint(4) DEFAULT NULL,
+  `status_custom_claim` tinyint(4) DEFAULT NULL,
+  `attempts` int(11) DEFAULT NULL,
+  `claims` longtext DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `challenge_winners` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `participant_id` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `prize` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
