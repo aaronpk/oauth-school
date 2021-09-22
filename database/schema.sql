6e6b7bfcebae1ce90badd5c17738ccf2a2932caf
Aaron Parecki <aaron@parecki.com>
2021-09-22 08:49:42 -0700

    switch to database sessions and fix session lifetime to 1 year

--- database/schema.sql
+++ database/schema.sql
@@ -48,3 +48,11 @@ CREATE TABLE `challenge_winners` (
   `archived` tinyint(4) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `sessions` (
+    `sess_id` VARBINARY(128) NOT NULL PRIMARY KEY,
+    `sess_data` BLOB NOT NULL,
+    `sess_lifetime` INTEGER UNSIGNED NOT NULL,
+    `sess_time` INTEGER UNSIGNED NOT NULL,
+    INDEX `sessions_sess_lifetime_idx` (`sess_lifetime`)
+) COLLATE utf8mb4_bin, ENGINE = InnoDB;
